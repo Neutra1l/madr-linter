2eeaa42f4cd28be8af76b24820329b264227e5dd
Neutra1l <viethung2012003@gmail.com>
2026-01-20 17:47:51 +0100

    Wording of diagnostics message of Rule 4 change.

--- app/src/main/java/neutra1/tool/rules/impl/Rule04.java
+++ app/src/main/java/neutra1/tool/rules/impl/Rule04.java
@@ -34,7 +34,7 @@ public class Rule04 extends SectionRule {
         int chosenOptionIndex = DecisionOutcomeElements.CHOSEN_OPTION.findIndexOfSubstringInText(chosenOptionText, true);
         int rationaleIndex = DecisionOutcomeElements.RATIONALE.findIndexOfSubstringInText(chosenOptionText, true);
         if (rationaleIndex == -1 || rationaleIndex < chosenOptionIndex) {
-            String description = "Rationale for the chosen option must be provided after stating the chosen option.\n";
+            String description = "Rationale for the chosen option must be provided directly after stating the chosen option.\n";
             int lineNumber = getLineNumberByContent(
                 getHeadingInfoByText(MandatorySection.DECISION_OUTCOME.getPermittedTitles()),
                 DecisionOutcomeElements.CHOSEN_OPTION.getKeywords());
