53abd9c9e9a6d08ca74097e4c31726012e479bb2
Neutra1l <viethung2012003@gmail.com>
2026-01-23 03:01:02 +0100

    Reworked Rule 02.

--- app/src/main/java/neutra1/linter/rules/impl/atomic/Rule02.java
+++ app/src/main/java/neutra1/linter/rules/impl/atomic/Rule02.java
@@ -1,5 +1,8 @@
 package neutra1.linter.rules.impl.atomic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import neutra1.linter.models.records.HeadingInfo;
 import neutra1.linter.models.records.Violation;
 import neutra1.linter.rules.SectionRule;
@@ -17,17 +20,22 @@ public class Rule02 extends SectionRule implements IAtomicRule {
 
     @Override
     public void check() {
+        List<HeadingInfo> emptyBodyHeadings = new ArrayList<>();
         for (HeadingInfo headingInfo : traverser.getHeadingInfoList()) {
             if (headingInfo.getBodyUnderHeading(true).isBlank()) {
-                reportEmptySection(headingInfo);
+                emptyBodyHeadings.add(headingInfo);
             }
         } 
-    }
-    
-    private void reportEmptySection(HeadingInfo headingInfo){
-        String description = "Section '" + headingInfo.text() + "' is empty.\n";
-        int lineNumber = headingInfo.startLineNumber();
-        reporter.report(new Violation(RULE_ID, description, lineNumber + 1));
+        if (!emptyBodyHeadings.isEmpty()){
+            StringBuilder desc = new StringBuilder("The following sections are empty:\n");
+            emptyBodyHeadings.forEach(headingInfo -> {
+                String heading = headingInfo.text();
+                int lineNumber = headingInfo.startLineNumber();
+                desc.append(LISTING_INDENT_SHORT + "Line " + lineNumber + ": " + heading + "\n");
+            }
+            );
+            reporter.report(new Violation(RULE_ID, desc.toString(), -1));
+        }
     }
 }
 
